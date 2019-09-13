651d12fc0d6152a8ccce0c4d33831a3f1feb5749
Alex Reed <alexreed100@gmail.com>
2019-09-13 13:07:17 -0400

    printDeck and createDeck methods added to main

--- main.java
+++ main.java
@@ -2,17 +2,26 @@ public class main
 { 
 	public static void main(String [] args)
 	{
-    Deck deck = new Deck();
-    String [] suits = {"hearts", "clubs", "diamonds", "spades"};
-    for(int i = 2; i<=14; i++){
-        for(int j = 0; j<suits.length; j++){
-            Card temp = new Card(i, suits[j]);
-            deck.addCard(temp);
-        }
+    Deck deck = createDeck();
+    printDeck(deck);
     }
-    deck.shuffle();
-    for(int i = 0; i<deck.getLength(); i++) {
-    	System.out.println(deck.getCard(i));
-    }
-    }
+	//Prints the cards in the deck, in order
+	public static void printDeck(Deck deck) {
+		for(int i = 0; i<deck.getLength(); i++) {
+	    	System.out.println(deck.getCard(i));
+	    }
+	}
+	//Creates and shuffles a standard 52 card deck
+	public static Deck createDeck() {
+		Deck deck = new Deck();
+	    String [] suits = {"hearts", "clubs", "diamonds", "spades"};
+	    for(int i = 2; i<=14; i++){
+	        for(int j = 0; j<suits.length; j++){
+	            Card temp = new Card(i, suits[j]);
+	            deck.addCard(temp);
+	        }
+	    }
+	    deck.shuffle();
+	    return deck;
+	}
 }
