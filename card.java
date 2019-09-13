81aba2251d3c46a0205390aba1646467bf6d530a
Alex Reed <alexreed100@gmail.com>
2019-09-13 00:33:23 -0400

    Constructor for card class

--- card.java
+++ card.java
@@ -1,9 +1,13 @@
 package randan;
 
+//A card has a rank (1-14) and a suit (hearts ,spades, diamonds, clubs)
 public class Card
 { 
-	public Card()
+    int rank;
+    String suit;
+	public Card(int rank, String suit)
 	{
-
+    this.rank = rank;
+    this.suit = suit;
     }
     }
--- main.java
+++ main.java
@@ -3,4 +3,5 @@ public class main
 	public static void main(String [] args)
 	{
 
-    }}
+    }
+}
