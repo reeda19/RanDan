da8c4a068e7e6de89d5692434e92420debcbe020
Alex Reed <alexreed100@gmail.com>
2019-09-14 19:07:50 -0400

    Started playerturn function

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class main
 { 
 	public static void main(String [] args)
@@ -17,6 +18,14 @@ public class main
     deck.dealCard(center);
     System.out.println("The first card dealt is the "+center.getCard(0).toString());
     System.out.println("The total count for the center is "+center.getTotal());
+    int player1_wins=0;
+    int player2_wins=0;
+    
+    //first to 3 wins
+    while (player1_wins<3 && player2_wins<3) {
+    	playerturn(player1, center);
+    	System.out.println("The total count for the center is "+center.getTotal());
+    }
     }
 	
 	
@@ -49,5 +58,12 @@ public class main
 			deck.dealCard(player1);
 			deck.dealCard(player2);
 		}
+	}
+	
+	public static void playerturn(Deck player, Deck center) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your move? Select a card (0-7)");
+		int num = input.nextInt();
+		player.dealCard(center);
 	}
 }
